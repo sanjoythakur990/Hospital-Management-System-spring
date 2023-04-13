ba0a9b186f503587229137c86f5f9c726ab7258b
Sanjoy <sanjoythakur990gmail.com>
2023-04-13 22:48:02 +0530

    Hospital Management System

---
+++ src/main/java/com/example/Hospital/Management/System/PatientController.java
@@ -0,0 +1,58 @@
+package com.example.Hospital.Management.System;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+public class PatientController {
+    HashMap<Integer,Patient> patientDb=new HashMap<>();
+    @PostMapping("/addPatientViaParameters")
+    public String addPatient(@RequestParam("patientId")Integer patientId,@RequestParam("name")String name,@RequestParam("age")Integer age,
+                             @RequestParam("disease")String disease){
+        Patient patient=new Patient(patientId,name,disease,age);
+        patientDb.put(patientId,patient);
+        return "Patient added successfully";
+    }
+
+    @PostMapping("/addPatientViaRequestBody")
+    public String addPatient(@RequestBody Patient patient){
+        int key=patient.getPatientId();
+        patientDb.put(key,patient);
+        return "Patient added via Request Body";
+    }
+
+    @GetMapping("/getPatientInfo")
+    public Patient getPatient(@RequestParam("patientId")Integer patientId){
+        Patient patient=patientDb.get(patientId);
+        return patient;
+    }
+
+    @GetMapping("/getAllPatients")
+    public List<Patient> getAllPatients(){
+        List<Patient> patients=new ArrayList<>();
+        for(Patient p:patientDb.values()){
+            patients.add(p);
+        }
+        return patients;
+    }
+
+    @GetMapping("/getPatientByName")
+    public Patient getPatientByName(@RequestParam("name")Integer name){
+        for(Patient p:patientDb.values()){
+            if(p.getName().equals(name))
+                return p;
+        }
+        return null;
+    }
+
+    @GetMapping("/getPatientsListGreaterThanAge")
+    public List<Patient> getPatientsGreaterThanAge(@RequestParam("age")Integer age){
+        List<Patient> list=new ArrayList<>();
+        for(Patient p:patientDb.values()){
+            if(p.getAge()>age)
+                list.add(p);
+        }
+        return list;
+    }
+}
